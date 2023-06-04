dd6f40275b85d9298ea5288487907ed4fda4f8a8
pgm-AnnaYoditOlsen <annayodit.olsen@student.arteveldehs.be>
2023-06-04 21:33:48 +0200

    Create fade in up animation

---
+++ static/scripts/animation/fade.js
@@ -0,0 +1,18 @@
+import gsap from 'gsap';
+
+const fadeElements = () => {
+    const $fadeElements = document.querySelectorAll('[data-animation="fade"]');
+
+    $fadeElements.forEach(($fadeElement) => {
+        gsap.from($fadeElement, {
+            opacity: 0,
+            y: 50,
+            scrollTrigger: {
+                trigger: $fadeElement,
+                start: 'top 80%',
+            }
+        })
+    })
+}
+
+export default fadeElements
