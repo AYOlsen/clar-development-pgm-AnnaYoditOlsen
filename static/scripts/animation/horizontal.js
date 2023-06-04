6bd4e2403406360e8bd0d01f300cc152b9f40379
pgm-AnnaYoditOlsen <annayodit.olsen@student.arteveldehs.be>
2023-06-04 21:33:10 +0200

    Pin horizontal scroll

--- static/scripts/animation/horizontal.js
+++ static/scripts/animation/horizontal.js
@@ -2,16 +2,20 @@ import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
 gsap.registerPlugin(ScrollTrigger);
 
-const $overflowContainer = document.querySelector("[data-animation='horizontal-scroll']");
-const $wideElement = $overflowContainer.querySelectorAll("[data-child='horizontal-scroll']");
+const pinElements = () => {
+    const $trigger = document.querySelector("[data-animation='pin-scroll']");
+    const $overflowElement = $trigger.querySelectorAll("[data-child='pin-scroll']");
 
-gsap.to($wideElement, {
-    x: (_, $wideElement) => - ($wideElement.scrollWidth - window.innerWidth / 4) - 500,
-    scrollTrigger: {
-        trigger: $overflowContainer,
-        start: 'top 30%',
-        end: 'bottom top',
-        scrub: true,
-        pin: true,
-    }
-});
+    gsap.to($overflowElement, {
+        x: (_, $overflowElement) => - ($overflowElement.scrollWidth - window.innerWidth / 4) - 500,
+        scrollTrigger: {
+            trigger: $trigger,
+            start: 'top 30%',
+            end: 'bottom top',
+            scrub: true,
+            pin: $trigger,
+        }
+    });
+}
+
+export default pinElements;
--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,6 +1,12 @@
 import './animation/horizontal.js';
 import './animation/scroll.js';
+import './animation/fade.js';
 
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
 gsap.registerPlugin(ScrollTrigger);
+
+import pinElements from './animation/horizontal.js';
+import fadeElements from './animation/fade.js';
+pinElements();
+fadeElements();
