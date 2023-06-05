165625035119b0ecaa9af7702a95cbf2e087fcf9
pgm-AnnaYoditOlsen <annayodit.olsen@student.arteveldehs.be>
2023-06-05 14:07:55 +0200

    Finish html

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,6 +1,8 @@
 import './animation/horizontal.js';
 import './animation/scroll.js';
 import './animation/fade.js';
+import './animation/slide-in.js';
+
 
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
@@ -8,5 +10,9 @@ gsap.registerPlugin(ScrollTrigger);
 
 import pinElements from './animation/horizontal.js';
 import fadeElements from './animation/fade.js';
+import {staggerLists}  from './animation/stagger.js';
+import slideInUp  from './animation/slide-in.js';
 pinElements();
 fadeElements();
+staggerLists();
+
